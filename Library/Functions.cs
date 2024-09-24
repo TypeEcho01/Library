deee62e52c40d862fca49ddfb7b3b3c5cc64bde1
TypeEcho01 <schwartz001@colum.edu>
2024-09-24 12:13:44 -0500

    Add project files.

---
+++ Library/Functions.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Library
+{
+    public static class Functions
+    {
+        // Attributes
+        private static readonly Printer _printer = new();
+        private static readonly Random _random = new();
+
+        // Print functions
+        public static void Print(params object?[] values) => _printer.Print(values);
+
+        public static void ConfigurePrint(string? sep = null, string? end = null, string? file = null)
+        {
+            if (sep is not null)
+                _printer.Sep = sep;
+
+            if (end is not null)
+                _printer.End = end;
+
+            if (file is not null)
+                _printer.File = file;
+        }
+
+        // Console functions
+        public static string Input()
+        {
+            string? output = Console.ReadLine();
+
+            if (output is not null)
+                return output;
+
+            return String.Empty;
+        }
+
+        public static string Input(string prompt)
+        {
+            Console.Write(prompt);
+            return Input();
+        }
+
+        public static void Pause() => Console.ReadKey();
+
+        public static void Pause(string message)
+        {
+            Console.WriteLine(message);
+            Pause();
+        }
+
+        public static void ClearScreen() => Console.Clear();
+
+        // StringTo functions
+        public static int StringToInt(string value)
+        {
+            if (int.TryParse(value, out int result))
+                return result;
+
+            throw new ArgumentException($"StringToInt(\"{value}\") cannot convert \"{value}\" to int");
+        }
+
+        public static int StringToInt(string value, int failsafe)
+        {
+            if (int.TryParse(value, out int result))
+                return result;
+
+            return failsafe;
+        }
+
+        public static float StringToFloat(string value)
+        {
+            if (float.TryParse(value, out float result))
+                return result;
+
+            throw new ArgumentException($"StringToFloat(\"{value}\") cannot convert \"{value}\" to float");
+        }
+
+        public static float StringToFloat(string value, float failsafe)
+        {
+            if (float.TryParse(value, out float result))
+                return result;
+
+            return failsafe;
+        }
+
+        public static double StringToDouble(string value)
+        {
+            if (double.TryParse(value, out double result))
+                return result;
+
+            throw new ArgumentException($"StringToDouble(\"{value}\") cannot convert \"{value}\" to double");
+        }
+
+        public static double StringToDouble(string value, double failsafe)
+        {
+            if (double.TryParse(value, out double result))
+                return result;
+
+            return failsafe;
+        }
+
+        // Random functions
+        public static double Random() => _random.NextDouble();
+
+        public static int RandRange(int stop) => _random.Next(stop);
+
+        public static int RandRange(int start, int stop) => _random.Next(start, stop);
+
+        public static int RandRange(int start, int stop, int step)
+        {
+            if (step <= 0)
+                throw new ArgumentOutOfRangeException(nameof(step), $"Step must be greater than 0, not {step}");
+
+            if (start >= stop)
+                throw new ArgumentOutOfRangeException(nameof(start), nameof(step),
+                    $"start must be less than stop, but {start} was {((start == stop) ? "equal to" : "greater than")} {stop}"
+                );
+
+            int rangeSize = (stop - start) / step;
+            if (rangeSize <= 0)
+                throw new ArgumentException($"The range is too small for the step, as ({stop} - {start}) / {step} == {rangeSize}");
+
+            int stepIndex = _random.Next(rangeSize + 1);
+            int result = start + (stepIndex * step);
+
+            return result;
+        }
+
+        public static int RandInt(int a, int b) => _random.Next(a, b + 1);
+
+        public static double Uniform(int a, int b) => a + _random.NextDouble() * (b - a);
+
+        public static T Choice<T>(T[] array)
+        {
+            if (array is null)
+                throw new ArgumentException("Array cannot be null", nameof(array));
+
+            if (array.Length == 0)
+                throw new ArgumentException("Array cannot be empty", nameof(array));
+
+            return array[_random.Next(array.Length)];
+        }
+    }
+}
---
+++ Library/Printer.cs
@@ -0,0 +1,177 @@
+﻿using System.Text;
+
+namespace Library
+{
+    public class Printer
+    {
+        // _nullRepresentation will Print instead of "" when null is passed
+        private static readonly string _nullRepresentation = "null";
+
+        // If File == _consoleRepresentation, uses Console.Out instead
+        private static readonly string _consoleRepresentation = "Console";
+
+        // Sep is the string that joins the values together
+        public string Sep;
+        private string _defaultSep;
+        public string DefaultSep
+        {
+            get => _defaultSep;
+            set
+            {
+                _defaultSep = value;
+                Sep = value; // Setting DefaultSep will update Sep too
+            }
+        }
+
+        // End is the final string of the output
+        public string End;
+        private string _defaultEnd;
+        public string DefaultEnd
+        {
+            get => _defaultEnd;
+            set
+            {
+                _defaultEnd = value;
+                End = value; // Setting DefaultEnd will update End too
+            }
+        }
+
+        // File is the file path that the output is printed to
+        public string File;
+        private string _defaultFile;
+        public string DefaultFile
+        {
+            get => _defaultFile;
+            set
+            {
+                _defaultFile = value;
+                File = value; // Setting DefaultFile will update File too
+                SetWriter(); // New _writer upon updating DefaultFile
+            }
+        }
+
+        /* _stringBuilder is only for GetOutput method, 
+         * but is an attribute to avoid constant instantiation */
+        private readonly StringBuilder _stringBuilder = new();
+
+        /* _writer is used to write to files when the default file is
+         * not Console.Out to stop repeated file opening
+         * Is not used when the file is changed temporarily */
+        private StreamWriter? _writer;
+
+        public Printer(string? sep = null, string? end = null, string? file = null)
+        {
+            /* Define default arguments here because:
+             * Environment.NewLine and _consoleRepresentation
+             * cannot be used as a default argument */
+            string defaultArgumentSep = " ";
+            string defaultArgumentEnd = Environment.NewLine;
+            string defaultArgumentFile = _consoleRepresentation;
+
+            Sep = sep ?? defaultArgumentSep;
+            _defaultSep = Sep;
+
+            End = end ?? defaultArgumentEnd;
+            _defaultEnd = End;
+
+            File = file ?? defaultArgumentFile;
+            _defaultFile = File;
+
+            SetWriter();
+        }
+
+        private void AddToStringBuilder(object? value)
+        {
+            if (value is not null)
+                _stringBuilder.Append(value);
+            else
+                _stringBuilder.Append(_nullRepresentation);
+
+            _stringBuilder.Append(Sep);
+        }
+
+        private void EndStringBuilder()
+        {
+            _stringBuilder.Length -= Sep.Length; // Removes the last Sep
+            _stringBuilder.Append(End);
+        }
+
+        private string GetStringBuilderResult()
+        {
+            string result = _stringBuilder.ToString();
+
+            // Clear the StringBuilder after every new output
+            _stringBuilder.Clear();
+
+            return result;
+        }
+
+        private string GetOutput(object?[] values)
+        {
+            if (values is null) // Sent null only
+                return _nullRepresentation + End;
+
+            if (values.Length == 0) // Sent nothing
+                return End;
+
+            foreach (object? value in values)
+            {
+                AddToStringBuilder(value);
+            }
+
+            EndStringBuilder();
+
+            return GetStringBuilderResult();
+        }
+
+        private void ResetAttributes()
+        {
+            Sep = _defaultSep;
+            End = _defaultEnd;
+            File = _defaultFile;
+        }
+
+        private void SetWriter()
+        {
+            _writer?.Dispose(); // Delete previous writer if exists
+
+            Stream stream;
+            if (File == _consoleRepresentation)
+                stream = Console.OpenStandardOutput();
+            else
+                stream = new FileStream(_defaultFile, FileMode.Append, FileAccess.Write, FileShare.Read);
+
+            _writer = new StreamWriter(stream)
+            {
+                AutoFlush = true
+            };
+        }
+
+        private void Write(string value)
+        {
+            // Use the _writer if the File is the same as the DefaultFile
+            if (File == _defaultFile)
+            {
+                _writer.Write(value);
+                return;
+            }
+
+            // Write to the console if the File is set to it
+            if (File == _consoleRepresentation)
+            {
+                Console.Write(value);
+                return;
+            }
+
+            // Write to the set File otherwise
+            System.IO.File.AppendAllText(File, value);
+        }
+
+        public void Print(params object?[] values)
+        {
+            string output = GetOutput(values);
+            Write(output);
+            ResetAttributes();
+        }
+    }
+}
